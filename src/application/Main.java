5d777d91995e896ce1675c88663d5045dbfa526e
Xavier Yick <xyick887@mtroyal.ca>
2023-03-28 22:40:29 -0600

    finished layout stuff, need to do code

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -11,8 +12,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("../view/View.fxml"));
-			Scene scene = new Scene(root,800,442);
+			TabPane root = (TabPane)FXMLLoader.load(getClass().getResource("../view/View.fxml"));
+			Scene scene = new Scene(root,800,400);
 			scene.getStylesheets().add(getClass().getResource("../view/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
